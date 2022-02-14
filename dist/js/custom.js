f00390a84ecaea6dd7bc3f6c7a952b0b7b78fdf4
SAMER MAJID AL-BAHABSA <62020378+Samer7777@users.noreply.github.com>
2022-02-14 22:56:16 +0200

    Update custom.js

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -51,3 +51,16 @@ faders.forEach((fader) => {
 sliders.forEach((slider) => {
   appearOnScroll.observe(slider);
 });
+
+// burger menu
+// const menuBtn = document.querySelector(".menu-btn");
+// let menuOpen = false;
+// menuBtn.addEventListener("click", () => {
+//   if (!menuOpen) {
+//     menuBtn.classList.add("open");
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove("open");
+//     menuOpen = false;
+//   }
+// });
